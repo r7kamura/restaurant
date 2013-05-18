0eb55dd2120ac9302f2f49239c43ffffddb936ed
Ryo Nakamura <r7kamura@gmail.com>
2013-05-18 14:26:49 +0900

    Use host method support instead of passing rack env

--- spec/requests/v1/recipes_spec.rb
+++ spec/requests/v1/recipes_spec.rb
@@ -1,12 +1,12 @@
 require "spec_helper"
 
 describe "/v1/recipes" do
-  let(:env) do
-    { "HTTP_ACCEPT" => "application/json" }
+  before do
+    self.accept = "application/json"
   end
 
   let(:recipe) do
-    post "/v1/recipes", { :recipe => { :title => "created" } }, env
+    post "/v1/recipes", { :recipe => { :title => "created" } }
     JSON.parse(response.body)
   end
 
@@ -20,7 +20,7 @@ describe "/v1/recipes" do
     end
 
     it "return recipes" do
-      get "/v1/recipes", nil, env
+      get "/v1/recipes"
       response.status.should == 200
       response.body.should be_json([recipe])
     end
@@ -28,7 +28,7 @@ describe "/v1/recipes" do
 
   describe "GET /v1/recipes/:id" do
     it "returns the recipe" do
-      get "/v1/recipes/#{id}", nil, env
+      get "/v1/recipes/#{id}"
       response.status.should == 200
       response.body.should be_json(recipe)
     end
@@ -36,20 +36,22 @@ describe "/v1/recipes" do
 
   describe "POST /v1/recipes" do
     it "creates a new recipe" do
-      post "/v1/recipes", { :recipe => { :title => "created" } }, env
+      post "/v1/recipes", { :recipe => { :title => "created" } }
       response.status.should == 201
       response.body.should be_json(
         "_id" => /\A[a-f0-9]{24}\z/,
         "title" => "created"
       )
+      id = JSON.parse(response.body)["_id"]
+      response.location.should == "http://www.example.com/v1/recipes/#{id}"
     end
   end
 
   describe "PUT /v1/recipes/:id" do
     it "updates the recipe" do
-      put "/v1/recipes/#{id}", { :recipe => { :title => "updated" } }, env
+      put "/v1/recipes/#{id}", { :recipe => { :title => "updated" } }
       response.status.should == 204
-      get "/v1/recipes/#{id}", nil, env
+      get "/v1/recipes/#{id}"
       response.body.should be_json(
         "_id" => id,
         "title" => "updated"
@@ -59,7 +61,7 @@ describe "/v1/recipes" do
 
   describe "DELETE /v1/recipes/:id" do
     it "deletes the recipe" do
-      delete "/v1/recipes/#{id}", nil, env
+      delete "/v1/recipes/#{id}"
       response.status.should == 204
     end
   end
