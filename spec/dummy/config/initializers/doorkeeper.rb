d12f2eb215159b27a062d744a59398c0d9e3bf59
Ryo Nakamura <r7kamura@gmail.com>
2013-05-18 22:51:52 +0900

    Change ORM to mongoid3 that doorkeeper will use

--- spec/dummy/config/initializers/doorkeeper.rb
+++ spec/dummy/config/initializers/doorkeeper.rb
@@ -1,7 +1,7 @@
 Doorkeeper.configure do
   # Change the ORM that doorkeeper will use.
   # Currently supported options are :active_record, :mongoid2, :mongoid3, :mongo_mapper
-  orm :active_record
+  orm :mongoid3
 
   # This block will be called to check whether the resource owner is authenticated or not.
   resource_owner_authenticator do
